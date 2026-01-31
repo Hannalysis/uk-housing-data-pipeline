82214e9d39eb5bd76faef155d017a422c9ffcb33
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-01-31 16:02:54 +0000

    feat: Added filter year script

---
+++ scripts/secondary_dataset/filter_year.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+processed_secondary_folder = PROJECT_ROOT / "data/processed/secondary"
+
+combined_ONS = pd.read_csv(processed_secondary_folder / 'combined_ONS.csv')
+
+# Year selection range
+START_YEAR = 2013
+END_YEAR = 2024
+
+# filter for years 2013–2024
+
+combined_ONS['year'] = combined_ONS['year'].astype(int)
+combined_ONS = combined_ONS[(combined_ONS['year'] >= START_YEAR) & (combined_ONS['year'] <= END_YEAR)]
+
+# Saving the filtered dataset
+combined_ONS.to_csv(processed_secondary_folder / 'combined_ONS_filtered_years.csv', index=False)
+
+print("Filtered ONS CSV shape:", combined_ONS.shape)
+print(combined_ONS.head())
